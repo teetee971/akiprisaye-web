3f08fa7ca0e2ff63c740109e07166ec5853797cb
teetee971 <ton@email.com>
2025-09-02 14:54:08 -0400

    feat: ajout config firebase complète

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AlzaSyBfQGLocAqVPNyk2w2Jyi0Pbej-Lz8tSYU",   // Clé API Web (vue dans ton écran)
-  authDomain: "a-ki-pri-sa-ye.firebaseapp.com",       // Toujours ce format
-  projectId: "a-ki-pri-sa-ye",                        // ID du projet
-  storageBucket: "a-ki-pri-sa-ye.appspot.com",        // Toujours ce format
-  messagingSenderId: "187272078809",                  // Numéro du projet
-  appId: "1:187272078809:android:a2841196fcd9735306e5c8" // ID de l'application
+  apiKey: "AlzaSyBfQGLocAqVPNyk2w2Jyi0Pbej-Lz8tSYU",
+  authDomain: "a-ki-pri-sa-ye.firebaseapp.com",
+  projectId: "a-ki-pri-sa-ye",
+  storageBucket: "a-ki-pri-sa-ye.appspot.com",
+  messagingSenderId: "187272078809",
+  appId: "1:187272078809:web:a2841196fcd9735306e5c8"
 };
 
 const app = initializeApp(firebaseConfig);
