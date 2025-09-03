d40b5a65a3ba567fa6d194fc89edae9d519f0cf2
teetee971 <ton@email.com>
2025-09-02 23:23:45 -0400

    deploy: Cloudflare Pages (redirects + .env + build)

---
+++ index.js
@@ -0,0 +1,16 @@
+// functions/eslint.config.cjs  (Flat config, CommonJS)
+
+module.exports = [
+  {
+    ignores: ['**/node_modules/**'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      ecmaVersion: 2021,
+    },
+    rules: {
+      'max-len': 'off',
+      'object-curly-spacing': 'off',
+      'comma-dangle': 'off',
+    },
+  },
+];
