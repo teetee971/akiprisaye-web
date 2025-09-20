3b5a7500b1016b72f7119043275bb4532c38d888
teetee971 <95961170+teetee971@users.noreply.github.com>
2025-09-20 11:55:53 -0400

    🔧 Fix Vite output directory for Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  css: { transformer: "postcss" }
-});
+export default {
+  build: {
+    outDir: "dist/public",
+  },
+};
