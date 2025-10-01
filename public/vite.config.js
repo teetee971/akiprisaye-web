6cc431a189ddf020c72e104ee1173b72b4214b77
teetee971 <95961170+teetee971@users.noreply.github.com>
2025-09-30 22:44:21 -0400

    fix: build vite + css static

---
+++ public/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
+
