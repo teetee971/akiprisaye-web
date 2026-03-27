2e9a7b47aa4f6f5a097179d19c42f4d3867160fa
teetee971 <thierrynaud2009@gmail.com>
2026-03-27 14:37:58 -0400

    chore: restore EspaceCreateur from main to fix corruption

--- vite.config.ts
+++ vite.config.ts
@@ -64,7 +64,6 @@ export default defineConfig(() => {
         '@': path.resolve(__dirname, 'src'),
       },
     },
-    build: {
       outDir: 'dist',
       emptyOutDir: true,
       sourcemap: false,
